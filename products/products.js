4cb6561b01bc7a137479d77cf717aea0341080f0
HungKimBku <kimhungtdblla24@gmail.com>
2021-11-16 00:52:07 +0700

    Hưng

--- products/products.js
+++
@@ -1,7 +0,0 @@
-function hover_img(id){
-    // var idx = ParseInt(id);
-    // var department = document.getElementsByClassName("department-link")[idx];
-    // var name = department.getElementsByClassName("department-card_name")[0];
-    // name.setAttribute(text-decoration,"underline");
-    alert("Ok");
-}
