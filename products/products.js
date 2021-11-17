f752537861ca682a9159216602030881d818b7f9
HungKimBku <kimhungtdblla24@gmail.com>
2021-11-18 00:40:42 +0700

    hưng

--- products/products.js
+++ products/products.js
@@ -1,4 +1,30 @@
-function rate_star()
+function rate_star(id, rate,text)
 {
-    alert('rate_star');
+    var node = document.getElementById(id);
+    var num = 0 ;
+    if(rate > 5) rate = 5;
+    for (var i = 1; i <= rate; i++) {
+        var star = document.createElement("span");
+        star.className = "review-rate-star fa fa-star";
+        star.style = "color:orange";
+        node.appendChild(star);
+        num ++;
+    }
+    for(var i = num; i < 5; i++){
+        var star = document.createElement("span");
+        star.className = "review-rate-star fa fa-star-o";
+        if(rate - num < 1) {
+            if(rate - num >= 0.5)
+                star.className = "review-rate-star fa fa-star-half-full";
+        }
+        star.style = "color:orange";
+        node.appendChild(star);
+        num ++;
+    }
+    var textReview = document.createElement("span");
+    textReview.className = "review-text";
+    textReview.style = "color:black; padding-left: 5px;";
+    textReview.innerText = '(' + text + ')';
+    node.appendChild(textReview);
+    
 }
