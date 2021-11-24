f949a273c55b8cd39d6cb8858d140c3050fdc3e7
HungKimBku <kimhungtdblla24@gmail.com>
2021-11-25 00:32:09 +0700

    HƯNG

--- house-self/custom/js/products.js
+++ house-self/custom/js/products.js
@@ -28,3 +28,33 @@ function rate_star(id, rate,text)
     node.appendChild(textReview);
     
 }
+function rate_star_2(id, rate,text)
+{
+    var node = document.getElementById(id);
+    var num = 0 ;
+    if(rate > 5) rate = 5;
+    for (var i = 1; i <= rate; i++) {
+        var star = document.createElement("span");
+        star.className = "review-rate-star fa fa-star";
+        star.style = "color:orange";
+        node.appendChild(star);
+        num ++;
+    }
+    for(var i = num; i < 5; i++){
+        var star = document.createElement("span");
+        star.className = "review-rate-star fa fa-star-o";
+        if(rate - num < 1) {
+            if(rate - num >= 0.5)
+                star.className = "review-rate-star fa fa-star-half-full";
+        }
+        star.style = "color:orange";
+        node.appendChild(star);
+        num ++;
+    }
+    var textReview = document.createElement("span");
+    textReview.className = "review-text";
+    textReview.style = "color:#1A9CB7; padding-left: 5px;";
+    textReview.innerText =" " + text + " đánh giá";
+    node.appendChild(textReview);
+    
+}
